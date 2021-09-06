00fd51863278ebb434962f4caac4337b09b50625
Belal <belal1997medhat@gmail.com>
2021-09-06 19:08:34 +0200

    Initial Commit

---
+++ GraphQL APP/GraphQL_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_APPApp.swift
+//  GraphQL APP
+//
+//  Created by belal medhat on 06/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
